8dc011cef40eae905d0d4d16e60720a870534c6b
itwzhangzx02 <195628213@qq.com>
2019-12-19 19:22:31 +0800

    delete *.xml from TEST package and set skip package test package

--- src/main/java/learn/UserMapper.java
+++ src/main/java/learn/UserMapper.java
@@ -1,7 +1,35 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package learn;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserMapper {
 
   public User selectUser(String id);
 
+  public User selectUserByIdAndName(String id,String name);
+
+  public User selectUserByArgs(String id,String name);
+
+  public void updateName(@Param("name") String name , @Param("id")String id);
+
+  void insert(User user);
+
+  void batchInsert(List<User> list);
 }
--- src/test/java/com/br/itwzhangzx02/learn/ConnectionTest.java
+++ src/test/java/com/br/itwzhangzx02/learn/ConnectionTest.java
@@ -1,3 +1,18 @@
+/**
+ *    Copyright 2009-2019 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.br.itwzhangzx02.learn;
 
 
@@ -7,25 +22,49 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class ConnectionTest {
-  @Test
-  public void test01() throws IOException {
+
+  private static SqlSession sqlSession;
+
+  @BeforeAll
+  public static void getSqlSession() throws IOException {
     String resource = "resources/mybatis-config.xml";
     InputStream inputStream = Resources.getResourceAsStream(resource);
     //1、创建SqlSessionFactory
     SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     //2、获取sqlSession
-    SqlSession sqlSession = sqlSessionFactory.openSession();
+     sqlSession = sqlSessionFactory.openSession(true);
+  }
+
+  @Test
+  public  void test01() throws IOException {
+
+      User user = getUser(sqlSession);
+      System.out.println(user);
+
+  }
+
+  private  User getUser(SqlSession sqlSession) {
     //3、获取mapper
     UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
     //4、执行数据库操作，并处理结果集
-    User user = userMapper.selectUser("10");
-    System.out.println(user);
+    return userMapper.selectUser("10");
+
+  }
+
+  @Test
+  public void testUpdate() throws IOException {
+    UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+    userMapper.updateName("zzx","10");
   }
 
 
